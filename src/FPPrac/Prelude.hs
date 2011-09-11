3026ff1513f9dd23915dabaf4ccdd8c8d5679133
Christiaan Baaij <christiaan.baaij@gmail.com>
2011-09-11 22:11:02 +0200

    Add numbers and prelude stuff that uses them

--- src/FPPrac/Prelude.hs
+++ src/FPPrac/Prelude.hs
@@ -1,4 +1,67 @@
 module FPPrac.Prelude
-  (
+  ( module Prelude
+  , Number
+  , (!)
+  , length
+  , take
+  , drop
+  , replicate
+  , code
+  , decode
+  , itoa
+  , atoi
+  , ftoa
+  , atof
   )
 where
+
+import Data.Char (ord,chr)
+import Prelude hiding (Int,Integer,Double,Float,length,take,drop,replicate)
+import qualified Prelude as P
+
+import FPPrac.Prelude.Number
+
+(!) :: [a] -> Number -> a
+xs ! (I i) = xs !! (fromInteger i)
+
+length :: [a] -> Number
+length = I . toInteger . P.length
+
+nil :: [a]
+nil = []
+
+take :: Number -> [a] -> [a]
+take (I i) xs = P.take (fromInteger i) xs
+take _     _  = error "take undefined for float"
+
+drop :: Number -> [a] -> [a]
+drop (I i) xs = P.drop (fromInteger i) xs
+drop _     _  = error "drop undefined for float"
+
+replicate :: Number -> a -> [a]
+replicate (I i) a = replicate (fromInteger i) a
+replicate _     _ = error "replicate undefined for float"
+
+code :: Char -> Number
+code c = if i < 256 then
+      I $ toInteger i
+    else
+      error $ "encode: Not an ASCII character: " ++ [c]
+  where
+    i = ord c
+
+decode :: Number -> Char
+decode (I i) | i < 256   = chr $ fromInteger i
+             | otherwise = error $ "decode: Not an ASCII value: " ++ show i
+
+itoa :: Number -> String
+itoa = show
+
+atoi :: String -> Number
+atoi = I . read
+
+ftoa :: Number -> String
+ftoa = show
+
+atof :: String -> Number
+atof = F . read
---
+++ src/FPPrac/Prelude/Number.hs
@@ -0,0 +1,106 @@
+module FPPrac.Prelude.Number
+where
+
+data Number
+  = I Integer
+  | F Float
+  deriving (Eq,Ord)
+
+instance Show Number where
+  show (I i) = show i
+  show (F f) = show f
+
+instance Num Number where
+  (I i1) + (I i2) = I (i1 + i2)
+  (F f1) + (F f2) = F (f1 + f2)
+  (I i1) + (F f2) = F ((fromInteger i1) + f2)
+  (F f1) + (I i2) = F (f1 + (fromInteger i2))
+  (I i1) * (I i2) = I (i1 * i2)
+  (F f1) * (F f2) = F (f1 * f2)
+  (I i1) * (F f2) = F ((fromInteger i1) * f2)
+  (F f1) * (I i2) = F (f1 * (fromInteger i2))
+  negate (I i)    = I (negate i)
+  negate (F f)    = F (negate f)
+  abs (I i)       = I (abs i)
+  abs (F f)       = F (abs f)
+  signum (I i)    = I (signum i)
+  signum (F f)    = F (signum f)
+  fromInteger     = I
+
+instance Real Number where
+  toRational (I i) = toRational i
+  toRational (F f) = toRational f
+
+instance Enum Number where
+  toEnum         = I . toInteger
+  fromEnum (I i) = fromInteger i
+
+instance Integral Number where
+  quotRem (I i1) (I i2) = let (i1',i2') = quotRem i1 i2 in (I i1', I i2')
+  quotRem (F _)      _  = error "quotRem: first argument is not an integer"
+  quotRem _      (F _)  = error "quotRem: second argument is not an integer"
+  divMod  (I i1) (I i2) = let (i1',i2') = divMod i1 i2 in (I i1', I i2')
+  divMod (F _)      _   = error "divMod: first argument is not an integer"
+  divMod _      (F _)   = error "divMod: second argument is not an integer"
+  toInteger (I i)       = i
+  toInteger (F f)       = error "Can not use 'toInteger' to convert float to integer"
+
+instance Fractional Number where
+  (/) (I i1) (I i2) = F $ (fromInteger i1) / (fromInteger i2)
+  (/) (F d1) (F d2) = F $ d1 / d2
+  (/) (F d1) (I i2) = F $ d1 / (fromInteger i2)
+  (/) (I i1) (F d2) = F $ (fromInteger i1) / d2
+  fromRational      = F . fromRational
+
+instance RealFrac Number where
+  properFraction (F f) = let (b,a) = properFraction f in (b, F a)
+  properFraction (I i) = let (b,a) = properFraction (fromIntegral i) in (b, F a)
+  truncate (F f)       = truncate f
+  truncate (I i)       = truncate (fromIntegral i)
+  round (F f)          = round f
+  round (I i)          = round (fromIntegral i)
+  ceiling (F f)        = ceiling f
+  ceiling (I i)        = ceiling (fromIntegral i)
+  floor (F f)          = floor f
+  floor (I i)          = floor (fromIntegral i)
+
+instance Floating Number where
+  pi                    = F pi
+  exp (F f)             = F (exp f)
+  exp (I i)             = F (exp $ fromIntegral i)
+  sqrt (F f)            = F (sqrt f)
+  sqrt (I i)            = F (sqrt $ fromIntegral i)
+  log (F f)             = F (log f)
+  log (I i)             = F (log $ fromIntegral i)
+  (F f1) ** (F f2)      = F (f1 ** f2)
+  (I i1) ** (I i2)      = F ((fromIntegral i1) ** (fromIntegral i2))
+  (F f1) ** (I i2)      = F (f1 ** (fromIntegral i2))
+  (I i1) ** (F f2)      = F ((fromIntegral i1) ** f2)
+  logBase (F f1) (F f2) = F (logBase f1 f2)
+  logBase (I i1) (I i2) = F (logBase (fromIntegral i1) (fromIntegral i2))
+  logBase (F f1) (I i2) = F (logBase f1 (fromIntegral i2))
+  logBase (I i1) (F f2) = F (logBase (fromIntegral i1) f2)
+  sin (F f)             = F (sin f)
+  sin (I i)             = F (sin $ fromIntegral i)
+  tan (F f)             = F (tan f)
+  tan (I i)             = F (tan $ fromIntegral i)
+  cos (F f)             = F (cos f)
+  cos (I i)             = F (cos $ fromIntegral i)
+  asin (F f)            = F (asin f)
+  asin (I i)            = F (asin $ fromIntegral i)
+  atan (F f)            = F (atan f)
+  atan (I i)            = F (atan $ fromIntegral i)
+  acos (F f)            = F (acos f)
+  acos (I i)            = F (acos $ fromIntegral i)
+  sinh (F f)            = F (sinh f)
+  sinh (I i)            = F (sinh $ fromIntegral i)
+  tanh (F f)            = F (tanh f)
+  tanh (I i)            = F (tanh $ fromIntegral i)
+  cosh (F f)            = F (cosh f)
+  cosh (I i)            = F (cosh $ fromIntegral i)
+  asinh (F f)           = F (asinh f)
+  asinh (I i)           = F (asinh $ fromIntegral i)
+  atanh (F f)           = F (atanh f)
+  atanh (I i)           = F (atanh $ fromIntegral i)
+  acosh (F f)           = F (acosh f)
+  acosh (I i)           = F (acosh $ fromIntegral i)
