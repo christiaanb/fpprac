f4bd0968b2ab1eb471579f9851524ca5b985c532
Christiaan Baaij <christiaan.baaij@gmail.com>
2012-09-28 11:07:54 +0300

    Fix paths

--- fp.bat
+++ fp.bat
@@ -55,8 +55,8 @@ IF NOT EXIST P:\haskell\ghc\package.cache (
 
 set ghc_package_path=P:\haskell\ghc;
 setx ghc_package_path P:\haskell\ghc;
-set path=%PATH%;P:\haskell\bin
-setx path P:\haskell\bin
-md %appdata%/ghc
-echo :set -XNoImplicitPrelude >> %appdata%/ghc/ghci.conf
-echo :set -NoMonomorphismRestriction >> %appdata%/ghc/ghci.conf
+set path=%PATH%;C:\Program Files (x86)\Haskell Platform\2012.2.0.0\lib\extralibs\bin;P:\haskell\bin
+setx path "C:\Program Files (x86)\Haskell Platform\2012.2.0.0\lib\extralibs\bin;P:\haskell\bin"
+md %appdata%\ghc
+echo :set -XNoImplicitPrelude >> %appdata%\ghc\ghci.conf
+echo :set -NoMonomorphismRestriction >> %appdata%\ghc\ghci.conf
