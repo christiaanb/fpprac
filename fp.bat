3f03949fa7eb8d35a9271e21f63b3b2444bc5011
Christiaan Baaij <christiaan.baaij@gmail.com>
2013-09-02 13:42:38 +0200

    Only create .ghci file on labmachines if it does not exist

--- fp.bat
+++ fp.bat
@@ -57,6 +57,15 @@ set ghc_package_path=P:\haskell\ghc;
 setx ghc_package_path P:\haskell\ghc;
 set path=%PATH%;C:\Program Files (x86)\Haskell Platform\2012.2.0.0\lib\extralibs\bin;P:\haskell\bin
 setx path "C:\Program Files (x86)\Haskell Platform\2012.2.0.0\lib\extralibs\bin;P:\haskell\bin"
-md %appdata%\ghc
-echo :set -XNoImplicitPrelude >> %appdata%\ghc\ghci.conf
-echo :set -NoMonomorphismRestriction >> %appdata%\ghc\ghci.conf
+
+IF NOT EXIST %appdata%\ghc\ghci.conf (
+  goto :make-ghci-conf
+) ELSE (
+  goto :make-ghci-conf-end
+)
+
+:make-ghci-conf
+  md %appdata%\ghc
+  echo :set -XNoImplicitPrelude >> %appdata%\ghc\ghci.conf
+  echo :set -NoMonomorphismRestriction >> %appdata%\ghc\ghci.conf
+:make-ghci-conf-end
